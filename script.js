f108f091fe0aa854199f33501396e56161305246
Alex Martinig <alexmartinig90@gmail.com>
2023-03-10 10:35:13 +0000

    added button to randomly generate background color

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
+var randomButton = document.getElementById("randomButton");
 
 function setGradient() {
   body.style.background =
@@ -10,8 +11,28 @@ function setGradient() {
     css.textContent = body.style.background + ";";
 }
 
+function randomColor() {
+  var letters = "0123456789ABCDEF";
+  var color = "#";
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return(color);
+}
+
+function setRandomGradient() {
+  var randomColor1 = randomColor();
+  var randomColor2 = randomColor();
+  body.style.background =
+    "linear-gradient(to right, " + randomColor1 + "," + randomColor2 + ")";
+
+    css.textContent = body.style.background + ";";
+}
+
 color1.addEventListener("input", setGradient);
 
 color2.addEventListener("input", setGradient);
+
+randomButton.addEventListener("click", setRandomGradient);
 
 body.onload = setGradient;
