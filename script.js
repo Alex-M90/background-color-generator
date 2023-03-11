25c39cbefacaaa411ffe923ddaa8717e2386462d
Alex-M90 <alexmartinig90@gmail.com>
2023-03-11 19:39:18 +0000

    Updated variables

--- script.js
+++ script.js
@@ -12,8 +12,8 @@ function setGradient() {
 }
 
 function randomColor() {
-  const letters = "0123456789ABCDEF";
-  const color = "#";
+  let letters = "0123456789ABCDEF";
+  let color = "#";
   for (let i = 0; i < 6; i++) {
     color += letters[Math.floor(Math.random() * 16)];
   }
